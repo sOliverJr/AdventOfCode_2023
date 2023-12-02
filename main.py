3e5067468d49250008bcb19995dd55e6acc34297
Oliver Seider <64847610+sOliverJr@users.noreply.github.com>
2023-12-02 21:34:33 +0100

    fixed main

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-from Day02.day02_part2 import *
+import datetime
+import os
 
+day = datetime.datetime.today().day
+
+if day < 10:
+    file_path = os.getcwd() + '/Day0' + str(day) + '/day0' + str(day)
+else:
+    file_path = os.getcwd() + '/Day0' + str(day) + '/day0' + str(day)
+
+print('--- Day ' + str(day) + ' ---')
+
+print("Part 1: ", end="")
+os.system('python3 ' + file_path + '_part1.py')
+
+print("Part 2: ", end="")
+os.system('python3 ' + file_path + '_part2.py')
