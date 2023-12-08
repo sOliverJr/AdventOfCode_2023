6e5e7df24d37d0a439a61aefc2f7ddfb37e70138
Oliver Seider <oliver.seider@dm.de>
2023-12-08 13:52:18 +0100

    day05 - part2v2

--- Day05/day05_part2v2.py
+++ Day05/day05_part2v2.py
@@ -1,8 +1,11 @@
 from alive_progress import alive_bar
+
+
 class Seed:
     def __init__(self, seed_range_start, seed_range_end):
         self.start = seed_range_start
         self.end = seed_range_end
+
 
 class Map:
     def __init__(self, destination_start, source_start, range_length):
@@ -11,42 +14,58 @@ class Map:
         self.range_length = range_length
         self.source_end = self.source_start + self.range_length -1
 
-    def calculate_range_that_fits_and_return_rest(self, seed_range: Seed):
+    def calculate_range_that_fits_and_return_rest(self, seed_ranges: list[Seed]):
         """
         Returns two Seed-objects. The first one did not fit, the second one did
         """
-        if self.source_start <= seed_range.start <= self.source_end:
-            # If entire seed-range fits in the map range
-            if seed_range.end <= self.source_end:
-                return [Seed(self.destination_start, seed_range)]
-            # If only part of the seeds fit in the map range
+        ranges_that_fitted = []
+        ranges_that_did_not_fit = []
+
+        for seed_range in seed_ranges:
+            if self.source_start <= seed_range.start <= self.source_end:
+                # If entire seed-range fits in the map range
+                if seed_range.end <= self.source_end:
+                    offset = seed_range.start - self.source_start
+                    delta = seed_range.end - seed_range.start
+                    ranges_that_fitted.append(Seed(self.destination_start + offset, self.destination_start + offset + delta))
+                # If only part of the seeds fit in the map range
+                else:
+                    overflow = seed_range.end - self.source_end
+                    delta = self.source_end - seed_range.start
+                    offset = seed_range.start - self.source_start
+                    first_element_that_does_not_fit = seed_range.end - overflow + 1
+                    ranges_that_fitted.append(Seed(self.destination_start + offset, self.destination_start + offset + delta))
+                    ranges_that_did_not_fit.append(Seed(first_element_that_does_not_fit, seed_range.end))
+            elif self.source_start <= seed_range.end <= self.source_end:
+                delta = seed_range.end - self.source_start
+                last_element_that_does_not_fit = self.source_start - 1
+                ranges_that_fitted.append(Seed(self.destination_start, self.destination_start + delta))
+                ranges_that_did_not_fit.append(Seed(seed_range.start, last_element_that_does_not_fit))
+            elif seed_range.start < self.source_start and seed_range.end > self.source_end:
+                # seed_range = 50   -   150
+                # map_range=     75 - 125
+                left_last_element_that_does_not_fit = self.source_start-1
+                right_first_element_that_does_not_fit = self.source_end+1
+                start_element_that_fits = self.destination_start
+                end_element_that_fits = self.destination_start + self.range_length - 1
+                ranges_that_fitted.append(Seed(start_element_that_fits, end_element_that_fits))
+                ranges_that_did_not_fit.append(Seed(seed_range.start, left_last_element_that_does_not_fit))
+                ranges_that_did_not_fit.append(Seed(right_first_element_that_does_not_fit, seed_range.end))
+            # If none of the seeds fit in the range
             else:
-                overflow = seed_range.end - self.source_end
-                first_element_that_does_not_fit = seed_range.end - overflow + 1
-                return [Seed(first_element_that_does_not_fit, seed_range.end), Seed(seed_range.start, seed_range.end-overflow)]
-        elif self.source_start <= seed_range.end <= self.source_end:
-            overflow = seed_range.end - self.source_start
-            last_element_that_does_not_fit = seed_range.end - overflow - 1
-            return [Seed(seed_range.start, last_element_that_does_not_fit),
-                    Seed(seed_range.end - overflow, seed_range.end)]
-        elif seed_range.start < self.source_start and seed_range.end > self.source_end:
-            print('ALL HOPE IS LOST')
-            raise Exception('exiting....')
-        # If none of the seeds fit in the range
-        else:
-            return [seed_range]
+                ranges_that_did_not_fit.append(seed_range)
+
+        return ranges_that_fitted, ranges_that_did_not_fit
 
 
 # Getting file as array of lines
-file_name = 'Day05/input_day05.txt'
+file_name = 'input_day05.txt'
 input_array = []
 file = open(file_name, 'r')
 for line in file:
     if 'map' not in line:
         input_array.append(line.strip())
 
-# Output = inf
-output = float('inf')
 
 # Get a list of seeds and their range
 seeds_array_unformatted = input_array[0].split(': ')[1].split(' ')
@@ -55,7 +74,7 @@ i = 0
 while i < len(seeds_array_unformatted):
     seed_starting_point = seeds_array_unformatted[i]
     seed_amount = seeds_array_unformatted[i+1]
-    seed_range_array.append(Seed(seed_starting_point, (seed_starting_point + seed_amount - 1)))
+    seed_range_array.append(Seed(int(seed_starting_point), (int(seed_starting_point) + int(seed_amount) - 1)))
     i += 2
 
 # Remove first two lines of input_file
@@ -74,31 +93,32 @@ for i, line in enumerate(input_array):
         line_values = line.split(' ')
         map_arr.append(Map(int(line_values[0]), int(line_values[1]), int(line_values[2])))
 
-seeds = {}
 
-def ka():
-    for map in map_arr:
-        response = map.calculate_range_that_fits_and_return_rest(seed_range)
-        if response[1] is None:
-            continue
-
-        new_calculated_range.extend(calculated_range[1])
-        if response[0] is None:
-            break
-        else:
-
-new_seed_range = []
-calculated_range = seed_range_array
-for map_arr in all_map_arr:
-    for seed_range in calculated_range:
-        new_calculated_range = []
-        unchanged_ranges = []
+def put_ranges_trough_mapping(ranges: list[Seed], mappings: list[Map]):
+    mapped_ranges = []
+    for mapping in mappings:
+        fitted_ranges, not_fitted_ranges = mapping.calculate_range_that_fits_and_return_rest(ranges)
+        mapped_ranges.extend(fitted_ranges)
+        ranges = not_fitted_ranges
+    mapped_ranges.extend(ranges)
+    return mapped_ranges
 
 
-        calculated_range = new_calculated_range
+final_ranges = []
+with alive_bar(len(seed_range_array)) as bar:
+    for seed_range in seed_range_array:
+        previous_range = [seed_range]
+        for map_set in all_map_arr:
+            previous_range = put_ranges_trough_mapping(previous_range, map_set)
+        final_ranges.extend(previous_range)
+        bar()
 
 
+# Output = inf
+output = float('inf')
 
-
+for range in final_ranges:
+    if range.start < output:
+        output = range.start
 
 print(output)
